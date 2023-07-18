1eb324468ba2faf734ead43688730dd1ae9c7e15
iamjooon2 <iamjooon2@gmail.com>
2023-07-18 16:48:51 +0900

    docs: 메인페이지 중앙 버튼 문구 변경

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/">
-            Docusaurus Tutorial - 5min ⏱️
+            홈페이지 바로가기
           </Link>
         </div>
       </div>
