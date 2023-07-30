cdfc563d12ea5ff1a12100cb0a6433850464bfec
Rosie <dev.kayoung@gmail.com>
2023-07-31 01:23:13 +0900

    chore: 블로그 썸네일 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,8 +62,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/zipgo-social-card.jpg',
       navbar: {
         title: 'ZIPGO',
         logo: {
