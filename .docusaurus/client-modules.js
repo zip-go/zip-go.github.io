379e187f0b6959df89275c86b3eca125859654eb
Rosie <dev.kayoung@gmail.com>
2023-07-30 21:10:37 +0900

    chore: node_modules 제외

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/jooon/dev/woowacourse/team-blog-zipgo/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/jooon/dev/woowacourse/team-blog-zipgo/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/jooon/dev/woowacourse/team-blog-zipgo/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/jooon/dev/woowacourse/team-blog-zipgo/src/css/custom.css'),
-];
