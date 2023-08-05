991b3777e7338297a7c257ddf318415646e0e07b
iamjooon2 <iamjooon2@gmail.com>
2023-08-05 17:46:41 +0900

    배포 페이지 추가 (#6)

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/">
+            to="https://zipgo.pet">
             홈페이지 바로가기
           </Link>
         </div>
